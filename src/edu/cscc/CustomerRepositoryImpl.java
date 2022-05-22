e80d05a7084a83d098bce249e5c04cccc27dd637
Alisha Stewart <astewart22@student.cscc.edu>
2022-05-22 18:26:40 -0400

    Creation of CustomerRepositoryImpl.

---
+++ src/edu/cscc/CustomerRepositoryImpl.java
@@ -0,0 +1,18 @@
+package edu.cscc;
+
+import java.util.UUID;
+
+public class CustomerRepositoryImpl {
+
+    public Customer create(Customer customer){return null;}
+
+    public Customer read(UUID accountNumber){return null;}
+
+    public Customer fundByEmailAddress(String emailAddress){return null;}
+
+    public void update(Customer customer){}
+
+    public boolean delete(UUID accountNumber){return false;}
+
+    public int count(){return 0;}
+}
